ccf27f41f41264297c6a486b958753243744f724
youngmo <youngmo0806@naver.com>
2021-07-13 17:53:38 +0900

    2021/07/13:Second

--- BikeCalc/AppDelegate.swift
+++ BikeCalc/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import GoogleMobileAds
-
+import  AdSupport
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,10 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        
+//        DeviceManager.shared.requestPermission()
+        
+        
         //Google Demo ID
         //배너광고 운영    ca-app-pub-7013457434188658~1568959665
         //배너광고 DemoId ca-app-pub-3940256099942544/2934735716
         GADMobileAds.sharedInstance().start(completionHandler: nil)
         return true
+        
     }
 }
--- BikeCalc/Common/DeviceManager.swift
+++ BikeCalc/Common/DeviceManager.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 import SystemConfiguration
-
+import AdSupport
+import AppTrackingTransparency
 
 class DeviceManager {
     static let shared: DeviceManager = DeviceManager()
@@ -18,14 +19,22 @@ class DeviceManager {
         }
     }
     
+//    var idfa: UUID {
+//        get{
+//            return ASIdentifierManager.shared().advertisingIdentifier
+//        }
+//    }
+    
+    
     var bikeState:Bool = true   //신차,중고차
     var bikePrice: Int = 0      //바이크 가액
     var bikeCC: Int = 0         //바이크 배기량
     var bikeYear: String = ""       //출고연식
     var bikeRate: Int = 0       //적용 세율
     var total: Int = 0          //총 세금
-    var adUnitID = "ca-app-pub-7013457434188658~1568959665" //
-                    
+//    var adUnitID = "ca-app-pub-3940256099942544/2934735716" //test
+    var adUnitID = "ca-app-pub-7013457434188658~1568959665" //prod
+    
     private func checkDeviceNetworkStatus() -> Bool {
             print("Check to Device Natwork Status....")
             var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
@@ -49,4 +58,38 @@ class DeviceManager {
             let ret = (isReachable && !needsConnection)
             return ret
     }
+    
+    
+    //IDFA 광고 추적자 권한
+    func requestPermission() {
+        
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization { status in
+                switch status {
+                case .authorized:   //인정받은
+                    // Tracking authorization dialog was shown
+                    // and we are authorized
+                    print("Authorized")
+                    // Now that we are authorized we can get the IDFA
+                    print(ASIdentifierManager.shared().advertisingIdentifier)
+                case .denied:   //거부
+                    // Tracking authorization dialog was
+                    // shown and permission is denied print("Denied")
+                    print("Denied")
+                case .notDetermined:    //결정되지 않은
+                    // Tracking authorization dialog has not been shown
+                    print("Not Determined")
+                case .restricted:   //한정된
+                    print("Restricted")
+                @unknown default:
+                    print("Unknown")
+                    
+                }
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+        
+    }
+
 }
